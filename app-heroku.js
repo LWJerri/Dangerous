53782b43a27c31cf41a2f0269d331a87c9328876
LWJerri <50290430+LWJerri@users.noreply.github.com>
2020-07-08 04:56:14 +0300

    Updated Procfile and add app-heroku.js

---
+++ app-heroku.js
@@ -0,0 +1,31 @@
+const Discord = require("discord.js");
+const bot = new Discord.Client();
+
+bot.on("ready", () => {
+    const guild = bot.guilds.cache.get(process.env.guildID);
+    
+    let readyMessage = process.env.startMessage
+        .replace(`$0`, bot.user.username)
+        .replace(`$1`, bot.guilds.cache.size)
+        .replace(`$2`, bot.users.cache.size)
+        .replace(`$3`, bot.channels.cache.size)
+        .replace(`$4`, guild.name)
+
+    console.log(readyMessage);
+    bot.user.setActivity(process.env.status, {"type": process.env.typeStatus});
+        
+    if(process.env.setDefault){
+        bot.user.setAvatar(guild.iconURL());
+        bot.user.setUsername(guild.name);
+    }
+});
+
+bot.on("message", async message => {
+    if(message.author.bot) return;
+    if(message.channel.id === process.env.memesChat){
+        message.react(process.env.reactGood);
+        message.react(process.env.reactShit);
+    }
+});
+
+bot.login(process.env.token);
