c851cbd7e54b104584b1f56532ede0c19abdb84c
LWJerri <50290430+LWJerri@users.noreply.github.com>
2020-07-16 16:55:19 +0300

    .

--- app-heroku.js
+++ app-heroku.js
@@ -29,7 +29,7 @@ bot.on("message", async message => {
 });
 
 bot.on("guildMemberAdd", member => {
-    member.roles.add([`${process.env.JoinRoleOne}`, `${process.env.JoinRoleTwo}`]);
+    member.roles.add(["621026454586654760", "621026147282452501"]);
 });
 
 bot.login(process.env.token);
